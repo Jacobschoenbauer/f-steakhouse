86acfa6349e1c1f945daedf31d12f0e14560c7ed
Jacobschoenbauer <jhwt65@gmail.com>
2023-05-08 20:36:54 -0500

    working on logo pic

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -1,41 +1,46 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import data from "../Data/Data.json";
-import logo from "./pic/steakguy1.jpg"
+import { Link } from "react-router-dom";
 import "../App.css";
+const logo = data.about.logoMan(({ alt, src }) => (
+  <div>
+    <img className="logo" src={src} alt={alt} />
+  </div>
+));
+
 export default class Nav extends Component {
- 
   render() {
-    
     const {
-     about:{
-      title,
-    telephone,
-    },
+      about: { title, telephone },
     } = data;
-    
 
-  return (
-    <div className="navbox">
-      <h1 className="mtitle"><div>{title}</div></h1>
-      <div ><img className="logo1" src={logo} alt="logo1"/></div>
-      <section className="navgate">
-        <div className="nav">
-        <Link className="home" to="/">Home</Link>
+    return (
+      <div className="navbox">
+        <h1 className="mtitle">
+          <div>{title}</div>
+        </h1>
+        <div>{logo}</div>
+        <section className="navgate">
+          <div className="nav">
+            <Link className="home" to="/">
+              Home
+            </Link>
 
-        <Link className="menu" to="/menu">Menu</Link>
+            <Link className="menu" to="/menu">
+              Menu
+            </Link>
 
-        <Link className="location" to="/map">Location</Link>
- 
-        </div>
-        <div className="num">
-          <a className="phone" href="tel:651-463-3726">
-            {telephone}
-          </a>
-        </div>
-      </section>
-    </div>
-  );
-}
-
+            <Link className="location" to="/map">
+              Location
+            </Link>
+          </div>
+          <div className="num">
+            <a className="phone" href="tel:651-463-3726">
+              {telephone}
+            </a>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
